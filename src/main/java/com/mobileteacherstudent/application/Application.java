0935ba2f08f9e9fb92b487ed35ddeb1a15d58f7b
Jakub Czajkowski <44880904+Czajsoon@users.noreply.github.com>
2022-01-23 00:44:37 +0100

    hello heroku

--- src/main/java/com/mobileteacherstudent/application/Application.java
+++ src/main/java/com/mobileteacherstudent/application/Application.java
@@ -2,6 +2,8 @@ package com.mobileteacherstudent.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,12 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+}
+
+@RestController
+class hello{
+	@GetMapping("/")
+	public String hello(){
+		return "hello heroku";
+	}
 }
